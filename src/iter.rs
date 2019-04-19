3c8ca30cb36e6de54b47f0bff6d06932774fdb74
SungYong Jang <jsy@adain.kr>
2019-04-19 18:22:26 +0900

    Iterator 트레잇으로 자신만의 반복자 만들기 할차례

--- src/iter.rs
+++ src/iter.rs
@@ -3,7 +3,108 @@
 //! Description :
 //!
 
+struct Counter {
+    count: u32,
+}
+
+impl Counter {
+    fn new() -> Counter {
+        Counter { count: 0 }
+    }
+}
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.count += 1;
+
+        if self.count < 6 {
+            Some(self.count)
+        }else{
+            None
+        }
+    }
+}
 
 pub fn run(){
+    let counter = Counter::new();
+    for count in counter.iter() {
+        println!("{}", count);
+    }
 
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker")},
+        Shoe { size: 13, style: String::from("sandal")},
+        Shoe { size: 10, style: String::from("boot")},
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    for shoe in in_my_size.iter() {
+        println!("{:#?}", shoe);
+    }
+}
+
+#[derive(PartialEq, Debug)]
+struct Shoe{
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_my_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter()
+        .filter(|s| s.size == shoe_size)
+        .collect()
+}
+
+#[test]
+fn filters_by_size(){
+    let shoes = vec![
+        Shoe { size: 10, style: String::from("sneaker")},
+        Shoe { size: 13, style: String::from("sandal")},
+        Shoe { size: 10, style: String::from("boot")},
+    ];
+
+    let in_my_size = shoes_in_my_size(shoes, 10);
+    assert_eq!(in_my_size, vec![
+        Shoe { size: 10, style: String::from("sneaker")},
+        Shoe { size: 10, style: String::from("boot")},
+    ]);
+}
+
+#[test]
+fn iterator_demonstration(){
+    let v1 = vec![1, 2, 3];
+
+    let mut v1_iter = v1.iter();
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+
+    v1_iter = v1.iter();
+
+    let mut index = 1;
+
+    // 소유권이 넘어감
+    for value in v1_iter{
+        assert_eq!(value, &index);
+        index += 1;
+    }
+
+    v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None);
+
+    v1_iter = v1.iter();
+    assert_eq!(v1_iter.next(), Some(&1));
+    let sum: i32 = v1_iter.sum();
+    assert_eq!(sum, 5);
+
+    v1_iter = v1.iter();
+    let v2: Vec<_> = v1_iter.map(|x| x + 1).collect();
+    assert_eq!(v2, vec![2, 3, 4]);
 }
